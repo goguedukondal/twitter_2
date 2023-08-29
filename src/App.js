27394cd26d6c130b70466dafb7696c8b4754ac99
GOGU EDUKONDAL <97788201+goguedukondal@users.noreply.github.com>
2023-08-29 22:15:14 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,19 +3,21 @@ import './App.css';
 import Feed from './components/Feed';
 import Leftsidebar from './components/Leftsidebar';
 import SearchPart from './components/SearchPart';
-import {useState} from 'react'
-import Login from './components/Login';
+// import {useState} from 'react'
+// import Login from './components/Login';
 function App() {
-  const [isLoggedIn ,setIsLoggedIn] = useState(false)
+  // const [isLoggedIn ,setIsLoggedIn] = useState(false)
   
   return (
     <div >
       {/* <Login/> */}
-     {isLoggedIn?<div className="App">
+     {/* {isLoggedIn?<div className="App"> */}
+     <div className="App">
      <Leftsidebar/>
      <Feed/>
      <SearchPart/>
-     </div>:<Login isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn}/>}
+     </div>
+     {/* :<Login isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn}/>} */}
     </div>
   );
 }
